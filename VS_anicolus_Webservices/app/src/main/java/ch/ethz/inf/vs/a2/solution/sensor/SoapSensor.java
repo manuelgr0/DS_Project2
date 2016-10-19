bacab47b0e9a68b57217e54b8a6be8986de29fac
manuelgr <manuelgr77@outlook.com>
2016-10-19 18:54:42 +0200

    SoapSensor: http request done but getting response error

--- VS_anicolus_Webservices/app/src/main/java/ch/ethz/inf/vs/a2/solution/sensor/SoapSensor.java
+++ VS_anicolus_Webservices/app/src/main/java/ch/ethz/inf/vs/a2/solution/sensor/SoapSensor.java
@@ -14,27 +14,29 @@ import ch.ethz.inf.vs.a2.http.RemoteServerConfiguration;
 import ch.ethz.inf.vs.a2.sensor.AbstractSensor;
 
 
-/*TODO USE THE GUIDE : https://code.tutsplus.com/tutorials/consuming-web-services-with-ksoap--mobile-21242
- *TODO in the guide he used VER11 instead of VER12 (just in case of a error) */
+//TODO USE THE GUIDE : https://code.tutsplus.com/tutorials/consuming-web-services-with-ksoap--mobile-21242
 
 public class SoapSensor extends AbstractSensor {
     private static final String NAMESPACE = "http://webservices.vslecture.vs.inf.ethz.ch/";
     private static final String MAIN_REQUEST_URL = "http://vslab.inf.ethz.ch:8080/SunSPOTWebServices/SunSPOTWebservice";
     private static final String SOAP_ACTION = "http://webservices.vslecture.vs.inf.ethz.ch/getSpot";
+    private static final String METHOD_NAME = "getSpot";
+
 
     @Override
     public String executeRequest() throws Exception {
-        String result = null;
+        String result;
 
         //Creating/Configuring the envelope
-        String methodname = "getSpot";
-        SoapObject request = new SoapObject(NAMESPACE, methodname);
+        SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
         request.addProperty("id", "Spot3");
         SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
         envelope.dotNet = true;
         //envelope.implicitTypes = true;
         //envelope.setAddAdornments(false);
         envelope.setOutputSoapObject(request);
+
+        Log.d("request:     " , request.toString());
 
         //Creating the request
         HttpTransportSE ht = new HttpTransportSE(MAIN_REQUEST_URL);
@@ -47,6 +49,7 @@ public class SoapSensor extends AbstractSensor {
             Log.d("hallo", "-------------");
 
             //Processing the response
+            Log.d("before response" , envelope.bodyIn.toString());
             SoapPrimitive resultsString = (SoapPrimitive) envelope.getResponse();
 
             Log.d("eeeeeeeeeeee", "eeeeeeeeeeeeee");
