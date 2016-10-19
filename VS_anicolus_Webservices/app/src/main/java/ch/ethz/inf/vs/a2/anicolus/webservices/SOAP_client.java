d3760d10054f9cd94fc55d61502bbeb7c3c810e8
manuelgr <manuelgr77@outlook.com>
2016-10-19 13:55:53 +0200

    manual soap implemented with bugs

--- VS_anicolus_Webservices/app/src/main/java/ch/ethz/inf/vs/a2/anicolus/webservices/SOAP_client.java
+++ VS_anicolus_Webservices/app/src/main/java/ch/ethz/inf/vs/a2/anicolus/webservices/SOAP_client.java
@@ -2,8 +2,11 @@ package ch.ethz.inf.vs.a2.anicolus.webservices;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import ch.ethz.inf.vs.a2.sensor.SensorListener;
+import ch.ethz.inf.vs.a2.solution.sensor.XmlSensor;
 
 public class SOAP_client extends AppCompatActivity implements SensorListener {
 
@@ -11,6 +14,16 @@ public class SOAP_client extends AppCompatActivity implements SensorListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soap_client);
+
+        Button button1 = (Button)findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                XmlSensor sensor1 = new XmlSensor();
+                sensor1.getTemperature();
+            }
+        });
+
+        Button button2 = (Button)findViewById(R.id.button2);
     }
 
     @Override
--- VS_anicolus_Webservices/app/src/main/java/ch/ethz/inf/vs/a2/solution/sensor/XmlSensor.java
+++ VS_anicolus_Webservices/app/src/main/java/ch/ethz/inf/vs/a2/solution/sensor/XmlSensor.java
@@ -1,11 +1,71 @@
 package ch.ethz.inf.vs.a2.solution.sensor;
 
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import ch.ethz.inf.vs.a2.sensor.AbstractSensor;
 
 public class XmlSensor extends AbstractSensor {
+    private final String targeturl = "http://vslab.inf.ethz.ch:8080/SunSPOTWebServices/SunSPOTWebservice";
+    private final String urlParameters = "";
+
     @Override
     public String executeRequest() throws Exception {
-        return null;
+        URL url;
+        HttpURLConnection connection = null;
+        try {
+            //Create connection
+            url = new URL("http://vslab.inf.ethz.ch:8080/SunSPOTWebServices/SunSPOTWebservice");
+            connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("POST");
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            //connection.setRequestProperty("Content-type", "text/xml; charset=utf-8");
+            connection.setRequestProperty("Content-Type", "text/xml");
+
+            //Send request
+            DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+            wr.writeBytes(urlParameters);
+            wr.flush();
+            wr.close();
+
+            //Get Response
+            InputStream is;
+            Log.i("response", "code=" + connection.getResponseCode());
+            if (connection.getResponseCode() <= 400) {
+                is = connection.getInputStream();
+            } else {
+              /* error from server */
+                is = connection.getErrorStream();
+            }
+            // is= connection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            String line;
+            StringBuffer response = new StringBuffer();
+            while ((line = rd.readLine()) != null) {
+                response.append(line);
+                response.append('\r');
+            }
+            rd.close();
+            Log.i("response", "" + response.toString());
+            return response.toString();
+        } catch (Exception e) {
+            Log.e("error https", "", e);
+            return null;
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+
     }
 
     @Override
